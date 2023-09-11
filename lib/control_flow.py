31d056be8e62e853a6c71bbd5d28bdda2b40c820
ubah feisal <feisalubah1@gmail.com>
2023-09-11 14:43:52 +0300

    done

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,40 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
-def admin_login(username, password):
-    # your code here
-    pass
+# def admin_login(username, password):
+#     # your code here
+#     pass
 
-def hows_the_weather(temperature):
-    # your code here
-    pass
+# def hows_the_weather(temperature):
+#     # your code here
+#     pass
 
-def fizzbuzz(num):
-    # your code here
-    pass
+# def fizzbuzz(num):
+#     # your code here
+#     pass
+
+# def calculator(operation, num1, num2):
+#     # your code here
+#     pass
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if (operation == "+"):
+        return num1 + num2
+    elif (operation == "-"):
+        return num1 - num
+    elif (operation == "*"):
+        return num1 * num
+    elif (operation == "/"):
+        if num2 != 0:
+            return num1 / num2
+        else: 
+            print ("Invalid operation")
+            
+    else:
+        return None
+    
+operation = input("Enter an operation (+,-,*,/) : ")
+num1 = int(input("Enter Num1: "))
+num2 = int(input("Enter Num2: "))
+
+result = calculator(operation, num1, num2)
+print(result)        
